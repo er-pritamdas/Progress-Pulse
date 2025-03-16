ca8642f76633e0c7c3d931b5944a544c2383bfa8
Pritam Das <er.pritamdas22@gmail.com>
2025-03-16 09:46:21 +0530

    Corrected Response codes

--- Server/controllers/User-controllers/newUserEntry.controller.js
+++ Server/controllers/User-controllers/newUserEntry.controller.js
@@ -12,22 +12,22 @@ const isUserPresent = asynchandler(
         const {username, email, password, phoneNumber} = req.body
 
         if(!username){
-            throw new ApiError(400, "please enter username")
+            throw new ApiError(400, "Username is required")
         }
         if(!email){
-            throw new ApiError(201, "please enter email")
+            throw new ApiError(201, "Email is required")
         }
         if(!password){
-            throw new ApiError(400, "please enter password")
+            throw new ApiError(400, "Password is required")
         }
         if(!phoneNumber){
-            throw new ApiError(400, "please enter phonenumber")
+            throw new ApiError(400, "Phone number is required")
         }
 
 
         const userExist = await RegisteredUsers.findOne({username: username})
         if(userExist){
-            throw new ApiError(400, "User Already Present")
+            throw new ApiError(409, "User Already Present")
         }
 
         next()
@@ -38,18 +38,24 @@ const isUserPresent = asynchandler(
 const newUserEntry = asynchandler(
     async (req, res, next) => {
         const {username, email, password, phoneNumber} = req.body
-        const createUser = await RegisteredUsers.create(
-            {
-                username: username,
-                email: email,
-                passwordHash: password,
-                phoneNumber: phoneNumber
-            }
-        )
+        try{
 
-        return res.status(201).json(
-            new ApiResponse(200, createUser, "User Registered successfully")
-        )
+            const createUser = await RegisteredUsers.create(
+                {
+                    username: username,
+                    email: email,
+                    passwordHash: password,
+                    phoneNumber: phoneNumber
+                }
+            )
+    
+            return res.status(201).json(
+                new ApiResponse(201, createUser, "User Registered successfully")
+            )
+
+        }catch(error){
+            throw new ApiError(409, error.message)
+        }
     }
 )
 
--- Server/src/app.js
+++ Server/src/app.js
@@ -9,12 +9,6 @@ const app = express()
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
-app.use((err, req, res, next) => {
-    res.status(err.statusCode || 500).json({
-        success: false,
-        message: err.message || "Internal Server Error",
-    });
-});
 
 
 // Routes
@@ -23,5 +17,16 @@ app.use((err, req, res, next) => {
 app.use("/api/v1/users/registered", registeredUserRoutes)
 
 // LoggedIn User Routes
+
+
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).json({
+        status: err.statusCode,
+        success: false,
+        message: err.message || "Internal Server Error",
+        errors: err.errors,
+    });
+});
 
 export default app
