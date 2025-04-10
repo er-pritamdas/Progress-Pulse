ee8ad5c8af4f7d556434ff108e4ec5d42c2858ed
Pritam Das <pritam.das@tatacommunications.com>
2025-04-10 16:53:22 -0700

    Icons Added

---
+++ Client/src/utils/Icons/EllipsisVertical.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function EllipsisVertical() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="h-4 w-4 lucide lucide-ellipsis-vertical-icon lucide-ellipsis-vertical"
+    >
+      <circle cx="12" cy="12" r="1" />
+      <circle cx="12" cy="5" r="1" />
+      <circle cx="12" cy="19" r="1" />
+    </svg>
+  );
+}
+
+export default EllipsisVertical;
