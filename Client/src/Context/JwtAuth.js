fc5c8e4307cfa812e2da51da078bb8f8b358a876
Jyotsna <er.jyotsnabharti@gmail.com>
2025-04-11 22:27:02 +0530

    files added

---
+++ Client/src/Context/JwtAuth.js
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+
+  const jwtValidation = (location) => {
+    if (location == "/" || location == "/dashboard")
+    {
+        const token = localStorage.getItem("token");
+        try{
+        axios.get('/api/v1/dashbord', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })}
+          catch (err) {
+            err.response.message = "Token Expired";
+          }
+          
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
---
+++ Server/routes/Dashboard-routes/dashboard.routes.js
@@ -0,0 +1,12 @@
+import { Router} from "express";
+
+
+const router = Router()
+
+router.route("/").post(jwtAuthorization)
+
+
+
+
+
+export default router
