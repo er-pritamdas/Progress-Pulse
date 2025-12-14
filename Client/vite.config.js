6ef65454019a6cd364d7bb1e9f563c8df5122092
Pritam <er.pritamdas22@gmail.com>
2025-12-14 10:17:22 +0530

    fix: Update Vite proxy target for  from  to .

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:8000'
+      '/api': 'http://database:8000'
     },
   },
   plugins: [react(),tailwindcss()],
