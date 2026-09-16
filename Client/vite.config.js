fa77ced1f093d010be13e18ee9dd6ae0b75328bb
Pritam <er.pritamdas22@gmail.com>
2026-09-16 08:53:51 +0530

    Preparing for Cloudflare Deployement

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server:{
     proxy:{
       // '/api': 'http://backend:8000'
-      '/api': 'http://localhost:8000'
+      // '/api': 'http://localhost:8000'
+      '/api': 'https://progress-pulse-8cdj.onrender.com/'
     },
   },
   plugins: [react(),tailwindcss()],
